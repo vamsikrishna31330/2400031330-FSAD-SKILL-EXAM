baa4ef6eac65503d84cf08c0c91c7820b265d893
vamsikrisna31330 <2400031330@kluniversity.in>
2026-03-11 22:39:47 +0530

    Maven Hibernate project with Course entity and ClientDemo

---
+++ src/main/java/com/klef/fsad/exam/Course.java
@@ -0,0 +1,49 @@
+package com.klef.fsad.exam;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="course")
+public class Course {
+  @Id
+  @GeneratedValue(strategy=GenerationType.IDENTITY)
+  private int id;
+  private String name;
+  private String description;
+  @Temporal(TemporalType.DATE)
+  private Date date;
+  private String status;
+  public int getId() {
+    return id;
+  }
+  public void setId(int id) {
+    this.id = id;
+  }
+  public String getName() {
+    return name;
+  }
+  public void setName(String name) {
+    this.name = name;
+  }
+  public String getDescription() {
+    return description;
+  }
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  public Date getDate() {
+    return date;
+  }
+  public void setDate(Date date) {
+    this.date = date;
+  }
+  public String getStatus() {
+    return status;
+  }
+  public void setStatus(String status) {
+    this.status = status;
+  }
+
+  // getters, setters, toString
+}
